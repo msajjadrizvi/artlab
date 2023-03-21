c171c2bce721c54a45ba43bdef42b85041116fdd
Zain-geekgenix <zain@geekgenix.com>
2023-03-21 20:46:24 +0500

    fix all

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./container/pages/Home/Home";
-import Assessment from "./container/pages/Assesment/Assessment";
-import Soon from "./container/pages/Soon/Soon";
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/Assessment" element={<Assessment />} />
-        <Route exact path="/Soon" element={<Soon />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
+
--- src/container/pages/Soon/Soon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Soon() {
-  return (
-    <div>Soon</div>
-  )
-}
-
-export default Soon
